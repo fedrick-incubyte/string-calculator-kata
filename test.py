bca7933ef542e0966a92c3b1cc7d3a9e87d37c42
Fedrick Nishant <fednish@gmail.com>
2026-04-22 13:07:55 +0530

    Add newline at end of test.py and ensure assertions for StringCalculator's add method

--- test.py
+++ test.py
@@ -2,3 +2,5 @@ from string_calculator.calculator import StringCalculator
 def test_add():
     calculator = StringCalculator()
     assert calculator.add("") == 0
+    assert calculator.add("1") == 1
+    
