ab702672854b3a53eda2d73237a2f6dc58eb46c2
Fedrick Nishant <fednish@gmail.com>
2026-04-22 13:16:23 +0530

    Fix assertion in test_add for StringCalculator to return correct sum for newline-separated inputs

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ def test_add():
     assert calculator.add("1") == 1
     assert calculator.add("1,2") == 3
     assert calculator.add("1,2,3") == 6
-    assert calculator.add("1\n2,3") == 10
+    assert calculator.add("1\n2,3") == 6
