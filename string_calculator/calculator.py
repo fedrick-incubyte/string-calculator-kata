e7065f981a48e119ea43f86d88dd901b047862c6
Fedrick Nishant <fednish@gmail.com>
2026-04-22 13:28:46 +0530

    Refactor add method in StringCalculator to improve delimiter handling and streamline input processing

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -2,8 +2,12 @@ class StringCalculator:
     def add(self, numbers: str):
         if numbers == "":
             return 0
+        
+        if numbers.startswith("//"):
+            delimiter = numbers[2]
+            numbers = numbers.split("\n", 1)[1]
+            parts = numbers.split(delimiter)
         else:
-            return sum(
-                int(num) if num.isdigit() else 0
-                for num in numbers.replace("\n", ",").split(",")
-            )
+            parts = numbers.replace("\n", ",").split(",")
+            
+        return sum(int(num) if num.isdigit() else 0 for num in parts)
