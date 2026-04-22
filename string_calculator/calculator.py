64953c7426a7de050cf1fbeedaa24fa1417076f2
Fedrick Nishant <fednish@gmail.com>
2026-04-22 13:10:09 +0530

    Refactor add method in StringCalculator to handle comma-separated inputs

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -2,4 +2,6 @@ class StringCalculator:
     def add(self, numbers: str):
         if numbers == "":
             return 0
+        if "," in numbers:
+            return sum(int(num) for num in numbers.split(","))
         return int(numbers)
